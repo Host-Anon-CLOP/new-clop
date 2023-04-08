d05e384208566ece985e04d1db2181c095a47384
Moonbutt <avtolet969@gmail.com>
2023-04-08 10:42:01 +0300

    Fixed webpack compiler errors/warnings

--- frontend/src/application/layout.js
+++ frontend/src/application/layout.js
@@ -4,24 +4,15 @@ import "@popperjs/core";
 import * as bootstrap from 'bootstrap';
 window.bootstrap = bootstrap;
 
-// const toastLiveExample = document.getElementById('liveToast');
-// const toast = new bootstrap.Toast(toastLiveExample);
-// toast.show();
-// console.log(toast);
-
 const toasts = document.getElementsByClassName("toast");
 
 for (let i = 0; i < toasts.length; i++) {
-    // setTimeout(() => {
-    //     const toast = new bootstrap.Toast(toasts[i]);
-    //     toast.show();
-    // }, i*50);
     const toast = new bootstrap.Toast(toasts[i]);
     toast.show();
 }
 
-const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+[...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
 
 function dismissAll() {
     for (let i = 0; i < toasts.length; i++) {
